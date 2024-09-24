439f161fcc1b82854d5b32aa9aad9ab95385e3aa
Елена <soruaska@gmail.com>
2024-09-24 21:28:14 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+total_tasks = 12 #всего задач
+hours_spent = 1.5 #всего часов
+Course_name = 'Python'
+one_homework = hours_spent/total_tasks # среднее время
+print ('Курс:',Course_name, ', всего задач: ', total_tasks,", всего часов: ", hours_spent,', среднее время выполнения: ', one_homework, ' часа')
